6b4d0b0a695b364340eb3f5a145943832b1360eb
Maisara <maisara.sherif.ms@gmail.com>
2025-09-28 00:40:14 +0400

    updated server configurations

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maisarasherif/Go-Microservices
+
+go 1.25.1
--- main.go
+++ main.go
@@ -1,20 +1,50 @@
 package main
 
 import (
-	"handlers"
+	"context"
 	"log" //http package for creating web servers
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
+
+	"github.com/maisarasherif/Go-Microservices/handlers"
 )
 
 func main() {
 
-	l := log.New(os.Stdout, "product api", log.LstdFlags)
+	l := log.New(os.Stdout, "\n product api: ", log.LstdFlags)
+
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9090", sm)
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Receieved Terminate, graceful shutdown \n", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 
 }
